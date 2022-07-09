6b39f810d2fc9ec8252adcf1dbce401d14016410
Thiago Neubern <76756810+ThiagoDEV17@users.noreply.github.com>
2022-07-09 04:07:17 -0300

    Class 7

--- adivinhacao/program.c
+++ adivinhacao/program.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Bem vindo ao nosso jogo de adivinhação!\n");
+
+    //Imprime o cabeçalo do programa
+    printf("*******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhacao! *\n");
+    printf("*******************************************\n");
+
+    int numerosecreto = 42;
+
+    // printf("O numero secreto e %d.\n", numerosecreto);
 }
