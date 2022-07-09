a620888e3e233c57963d4008fa50d4ac544c9fcc
Thiago Neubern <76756810+ThiagoDEV17@users.noreply.github.com>
2022-07-09 04:19:11 -0300

    First exercises

---
+++ exe2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    int y;
+
+    printf("Digite um numero: ");
+    scanf("%d", &x);
+
+    printf("Digite outro numero: ");
+    scanf("%d", &y);
+
+    printf("A multiplicacao deles vale: %d", x * y);
+}
